b325b84698eee52567ef7f91f174726782d50e53
Rishav Saigal <rishavsaigal@gmail.com>
2024-02-19 17:23:52 +0530

    Chat feature completed & download history is there

--- layout/ask_resume.py
+++ layout/ask_resume.py
@@ -1,35 +1,35 @@
 """
 This file contains all the ask resume tab layout for the UI.
 """
-import os
-import time
+import json
 import streamlit as st
-import pandas as pd
-from utils.common import extract_pages_from_pdf, generate_ask_resume_results
+from utils.common import extract_pages_from_pdf, generate_ask_resume_results, write_file_to_pdf
 
 
 def get_layout_ask_resume(file_upload_var, GEMINI_AI):
   """
   This function returns the ask resume tab.
   """
+  question = st.text_input("Ask your resume ?")
 
-  question = st.text_input("Ask your resume ?", placeholder="Enter your query here")
+  if "messages" not in st.session_state:
+      st.session_state.messages = []
+
+  st.download_button("Download Chat", json.dumps(st.session_state.messages), "chat.json", "json")
+
+  for message in st.session_state.messages:
+      with st.chat_message(message["role"]):
+          st.markdown(message["content"])
+
   if question:
-    progress_bar = st.progress(0, 'Generating the answer ...')
+    st.session_state.messages.append({"role": "human", "content": question})
+    with st.chat_message("human"):
+      st.write(question)
 
-    if not os.path.exists('./temp'):
-        os.makedirs('./temp')
-
-    with open('./temp/temp.pdf', mode='wb') as file:
-        file.write(file_upload_var.getvalue())
-    EXTRACT_PAGES = extract_pages_from_pdf('./temp/temp.pdf')
-
-    output = generate_ask_resume_results(GEMINI_AI, EXTRACT_PAGES, question)
-    progress_bar.progress(100, 'Completed')
-    time.sleep(1)
-    progress_bar.empty()
-
-    st.markdown("<h5>Results</h5>", unsafe_allow_html=True)
-    st.markdown(output, unsafe_allow_html=True)
-  else:
-    st.info("Please enter the query")
+    with st.chat_message("ai"):
+      with st.spinner("Generating the answer ...."):
+        write_file_to_pdf(file_upload_var)
+        EXTRACT_RESUME_PAGES = extract_pages_from_pdf('./temp/temp.pdf')
+        output = generate_ask_resume_results(GEMINI_AI, EXTRACT_RESUME_PAGES, question)
+        st.session_state.messages.append({"role": "ai", "content": output})
+        st.markdown(output)
